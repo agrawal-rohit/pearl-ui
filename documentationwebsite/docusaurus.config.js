dd2cb2fd4d65af9f04356f49b797bda2ca939cf6
Rohit Agrawal <rohitagrawalofficialmail@gmail.com>
2021-09-10 13:54:16 +0530

    Add starter data for component docs

--- documentationwebsite/docusaurus.config.js
+++ documentationwebsite/docusaurus.config.js
@@ -4,17 +4,18 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 // With JSDoc @type annotations, IDEs can provide config autocompletion
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 (module.exports = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
+  title: "Pearl UI",
+  tagline: "Blazing fast mobile development",
   url: "https://your-docusaurus-test-site.com",
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "agrawal-rohit", // Usually your GitHub org/user name.
+  projectName: "pearl-ui", // Usually your repo name.
 
   plugins: ["docusaurus-plugin-react-native-web"],
+  themes: ["@docusaurus/theme-live-codeblock"],
 
   presets: [
     [
@@ -24,7 +25,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl: "https://github.com/facebook/docusaurus/edit/main/website/",
+          editUrl: "https://github.com/agrawal-rohit/pearl-ui",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
@@ -44,10 +45,9 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
         },
         items: [
           {
-            type: "doc",
-            docId: "introduction",
-            position: "left",
+            to: "/docs/introduction", // ./docs/Intro.md
             label: "Docs",
+            position: "left",
           },
           {
             href: "https://github.com/agrawal-rohit/pearl-ui",
