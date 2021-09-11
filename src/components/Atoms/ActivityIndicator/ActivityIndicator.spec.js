3fc44311688dfa89a91386392a78552bfbac8682
Rohit Agrawal <rohitagrawalofficialmail@gmail.com>
2021-09-11 15:06:32 +0530

    refactor(activityindicator): Switched to a dynamic component style for better readability, added tests and increased coverage

--- src/components/Atoms/ActivityIndicator/ActivityIndicator.spec.js
+++ src/components/Atoms/ActivityIndicator/ActivityIndicator.spec.js
@@ -30,4 +30,17 @@ describe("Atoms/ActivityIndicator", () => {
     const tree = comp.toJSON();
     expect(tree.children.length).toBe(1);
   });
+
+  it("overrides the color when specified", () => {
+    let comp;
+    renderer.act(() => {
+      comp = renderer.create(
+        <ThemeProvider>
+          <ActivityIndicator color="#7fff00" />
+        </ThemeProvider>
+      );
+    });
+    const tree = comp.toJSON();
+    expect(tree.children[0].props.color).toBe("#7fff00");
+  });
 });
