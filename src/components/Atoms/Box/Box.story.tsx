b3bf301a8ff4d9bdb3a2234dc185422aa9d74af7
Rohit Agrawal <rohitagrawalofficialmail@gmail.com>
2021-09-14 10:51:26 +0530

    Writing faulty box component

--- src/components/Atoms/Box/Box.story.tsx
+++ src/components/Atoms/Box/Box.story.tsx
@@ -2,12 +2,9 @@ import React from "react";
 import { storiesOf } from "@storybook/react-native";
 import Screen from "../Screen/Screen";
 import Box from "../Box/Box";
-import Text from "../Text/Text";
 
 storiesOf("Box", module)
   .addDecorator((getStory) => <Screen>{getStory()}</Screen>)
   .add("Basic box", () => (
-    <Box>
-      <Text variant="p1">This is a sample box</Text>
-    </Box>
+    <Box padding="s" width={200} backgroundColor="primary100"></Box>
   ));
