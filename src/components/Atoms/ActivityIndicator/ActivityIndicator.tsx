9c8e36acdabb4f54b769f2bd134d95bc03cc4def
Rohit Agrawal <rohitagrawalofficialmail@gmail.com>
2021-09-14 16:52:43 +0530

    refactor(typesafety): figured out dynamic typing using the base theme

--- src/components/Atoms/ActivityIndicator/ActivityIndicator.tsx
+++ src/components/Atoms/ActivityIndicator/ActivityIndicator.tsx
@@ -1,77 +1,78 @@
-import {
-  layout,
-  LayoutProps,
-  spacing,
-  SpacingProps,
-  useTheme,
-  useRestyle,
-} from "@shopify/restyle";
-import React from "react";
-import {
-  BallIndicator,
-  BarIndicator,
-  DotIndicator,
-  MaterialIndicator,
-  PacmanIndicator,
-  PulseIndicator,
-  SkypeIndicator,
-  UIActivityIndicator,
-  WaveIndicator,
-} from "react-native-indicators";
-import { PearlTheme } from "../../../theme/src/theme";
+// import React from "react";
+// import {
+//   BallIndicator,
+//   BarIndicator,
+//   DotIndicator,
+//   MaterialIndicator,
+//   PacmanIndicator,
+//   PulseIndicator,
+//   SkypeIndicator,
+//   UIActivityIndicator,
+//   WaveIndicator,
+// } from "react-native-indicators";
+// import useStyledProps from "../../../hooks/useStyledProps";
+// import { useTheme } from "../../../hooks/useTheme";
+// import { PearlTheme } from "../../../theme/src/basetheme";
+// import {
+//   layout,
+//   LayoutProps,
+//   spacing,
+//   SpacingProps,
+// } from "../../../theme/src/styleFunctions";
+// import { IBasePearlTheme } from "../../../theme/src/types";
 
-const restyleFunctions = [spacing, layout];
-type IActivityIndicatorProps = SpacingProps<PearlTheme> &
-  LayoutProps<PearlTheme> & {
-    color?: string;
-    size?: "s" | "m" | "l" | "xl";
-    variant?:
-      | "ball"
-      | "bar"
-      | "dot"
-      | "spinner"
-      | "pacman"
-      | "pulse"
-      | "skype"
-      | "activity"
-      | "wave";
-    loading?: boolean;
-  };
+// const restyleFunctions = [spacing, layout];
+// type IActivityIndicatorProps = SpacingProps<IBasePearlTheme> &
+//   LayoutProps & {
+//     color?: string;
+//     size?: "s" | "m" | "l" | "xl";
+//     variant?:
+//       | "ball"
+//       | "bar"
+//       | "dot"
+//       | "spinner"
+//       | "pacman"
+//       | "pulse"
+//       | "skype"
+//       | "activity"
+//       | "wave";
+//     loading?: boolean;
+//   };
 
-const IndicatorTypeToComponentMap = {
-  ball: BallIndicator,
-  bar: BarIndicator,
-  dot: DotIndicator,
-  spinner: MaterialIndicator,
-  pacman: PacmanIndicator,
-  pulse: PulseIndicator,
-  skype: SkypeIndicator,
-  activity: UIActivityIndicator,
-  wave: WaveIndicator,
-};
+// const IndicatorTypeToComponentMap = {
+//   ball: BallIndicator,
+//   bar: BarIndicator,
+//   dot: DotIndicator,
+//   spinner: MaterialIndicator,
+//   pacman: PacmanIndicator,
+//   pulse: PulseIndicator,
+//   skype: SkypeIndicator,
+//   activity: UIActivityIndicator,
+//   wave: WaveIndicator,
+// };
 
-const ActivityIndicator: React.FC<IActivityIndicatorProps> = ({
-  color = undefined,
-  size = "m",
-  variant = "spinner",
-  loading = true,
-  ...rest
-}) => {
-  if (!loading) return null;
+// const ActivityIndicator: React.FC<IActivityIndicatorProps> = ({
+//   color = undefined,
+//   size = "m",
+//   variant = "spinner",
+//   loading = true,
+//   ...rest
+// }) => {
+//   if (!loading) return null;
 
-  const theme = useTheme<PearlTheme>();
-  const props = useRestyle(restyleFunctions, rest);
+//   const { theme } = useTheme();
+//   const props = useStyledProps(restyleFunctions, rest);
 
-  const { primary500 } = theme.colors;
+//   const { primary500 } = theme.colors;
 
-  return React.createElement(IndicatorTypeToComponentMap[variant], {
-    color: color ? color : primary500,
-    size:
-      variant === "dot"
-        ? theme.activityIndicatorSize[size].size / 5
-        : theme.activityIndicatorSize[size].size,
-    ...props,
-  });
-};
+//   return React.createElement(IndicatorTypeToComponentMap[variant], {
+//     color: color ? color : primary500,
+//     size:
+//       variant === "dot"
+//         ? theme.activityIndicatorSize[size].size / 5
+//         : theme.activityIndicatorSize[size].size,
+//     ...props,
+//   });
+// };
 
-export default ActivityIndicator;
+// export default ActivityIndicator;
--- src/theme/src/themeContext.tsx
+++ src/theme/src/themeContext.tsx
@@ -1,27 +1,31 @@
-import React, { useEffect, useState, useContext, createContext } from "react";
+import React, { useEffect, useState, createContext } from "react";
 import { useColorScheme } from "react-native";
 import { ThemeProvider as RestyleThemeProvider } from "@shopify/restyle";
-import { baseLightTheme, baseDarkTheme } from "./theme";
+import { baseLightTheme, baseDarkTheme } from "./basetheme";
 import { IBasePearlTheme } from "./types";
 
 type ThemeType = "light" | "dark" | "system";
 
-export interface IThemeContext {
+export interface IThemeContext<
+  Theme extends IBasePearlTheme = IBasePearlTheme
+> {
   /** Theme configuration object for the active color mode */
-  theme: IBasePearlTheme;
+  theme: Theme;
   /** Active color mode */
   colorMode: ThemeType;
   /** Function to toggle the active color mode and corresponding theme */
   toggleTheme(): void;
 }
 
-interface ThemeProviderProps {
+interface ThemeProviderProps<Theme extends IBasePearlTheme = IBasePearlTheme> {
   /** Initial color mode for the theme (light, dark, system) */
   initialColorMode?: ThemeType;
   /** The configuration object for light theme */
-  lightTheme?: IBasePearlTheme;
+  lightTheme?: Theme;
   /** The configuration object for dark theme */
-  darkTheme?: IBasePearlTheme;
+  darkTheme?: Theme;
+  /**React children */
+  children: React.ReactNode;
 }
 
 export const themeContext = createContext({} as IThemeContext);
@@ -29,14 +33,14 @@ export const themeContext = createContext({} as IThemeContext);
 /**
  * The main provider component which controls the theme and color mode of the components in the entire application.
  */
-export const ThemeProvider: React.FC<ThemeProviderProps> = ({
+export const ThemeProvider = ({
   initialColorMode = "light",
   lightTheme = baseLightTheme,
   darkTheme = baseDarkTheme,
   children,
-}) => {
+}: ThemeProviderProps) => {
   const [colorMode, setColorMode] = useState<ThemeType>(initialColorMode);
-  const [activeTheme, setActiveTheme] = useState<IBasePearlTheme>(lightTheme);
+  const [activeTheme, setActiveTheme] = useState<typeof lightTheme>(lightTheme);
   const systemThemeStyle = useColorScheme() as ThemeType;
 
   const toggleTheme = () => {
@@ -51,7 +55,6 @@ export const ThemeProvider: React.FC<ThemeProviderProps> = ({
 
   useEffect(() => {
     // Set initial Theme
-
     if (colorMode === "light") {
       setColorMode("light");
       setActiveTheme(lightTheme);
@@ -68,9 +71,7 @@ export const ThemeProvider: React.FC<ThemeProviderProps> = ({
     <themeContext.Provider
       value={{ theme: activeTheme, colorMode, toggleTheme } as IThemeContext}
     >
-      <RestyleThemeProvider theme={activeTheme}>
-        {children}
-      </RestyleThemeProvider>
+      {children}
     </themeContext.Provider>
   );
 };
