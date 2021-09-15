4a5a9d704d10df992a9d9f3128e27e610c096157
Rohit Agrawal <rohit.agrawal@electriphi.ai>
2021-09-15 14:03:14 +0530

    feat(componentconfig): added component specific configuration to the theme

--- src/components/Atoms/ActivityIndicator/ActivityIndicator.tsx
+++ src/components/Atoms/ActivityIndicator/ActivityIndicator.tsx
@@ -1,78 +1,78 @@
-// import React from "react";
-// import {
-//   BallIndicator,
-//   BarIndicator,
-//   DotIndicator,
-//   MaterialIndicator,
-//   PacmanIndicator,
-//   PulseIndicator,
-//   SkypeIndicator,
-//   UIActivityIndicator,
-//   WaveIndicator,
-// } from "react-native-indicators";
-// import useStyledProps from "../../../hooks/useStyledProps";
-// import { useTheme } from "../../../hooks/useTheme";
-// import { PearlTheme } from "../../../theme/src/basetheme";
-// import {
-//   layout,
-//   LayoutProps,
-//   spacing,
-//   SpacingProps,
-// } from "../../../theme/src/styleFunctions";
-// import { IBasePearlTheme } from "../../../theme/src/types";
+import React from "react";
+import {
+  BallIndicator,
+  BarIndicator,
+  DotIndicator,
+  MaterialIndicator,
+  PacmanIndicator,
+  PulseIndicator,
+  SkypeIndicator,
+  UIActivityIndicator,
+  WaveIndicator,
+} from "react-native-indicators";
+import useStyledProps from "../../../hooks/useStyledProps";
+import { useTheme } from "../../../hooks/useTheme";
+import {
+  color,
+  layout,
+  LayoutProps,
+  spacing,
+  SpacingProps,
+} from "../../../theme/src/styleFunctions";
+import { BasePearlTheme } from "../../../theme/src/types";
+import { baseLightTheme } from "../../../theme/src/basetheme";
+import useComponentConfig from "../../../hooks/useComponentConfig";
 
-// const restyleFunctions = [spacing, layout];
-// type IActivityIndicatorProps = SpacingProps<IBasePearlTheme> &
-//   LayoutProps & {
-//     color?: string;
-//     size?: "s" | "m" | "l" | "xl";
-//     variant?:
-//       | "ball"
-//       | "bar"
-//       | "dot"
-//       | "spinner"
-//       | "pacman"
-//       | "pulse"
-//       | "skype"
-//       | "activity"
-//       | "wave";
-//     loading?: boolean;
-//   };
+const indicatorStyleFunctions = [color, spacing, layout];
+type IActivityIndicatorProps = SpacingProps<BasePearlTheme> &
+  LayoutProps & {
+    size?: keyof typeof baseLightTheme["components"]["ActivityIndicator"]["sizes"];
+    variant?:
+      | "ball"
+      | "bar"
+      | "dot"
+      | "spinner"
+      | "pacman"
+      | "pulse"
+      | "skype"
+      | "activity"
+      | "wave";
+    loading?: boolean;
+  };
 
-// const IndicatorTypeToComponentMap = {
-//   ball: BallIndicator,
-//   bar: BarIndicator,
-//   dot: DotIndicator,
-//   spinner: MaterialIndicator,
-//   pacman: PacmanIndicator,
-//   pulse: PulseIndicator,
-//   skype: SkypeIndicator,
-//   activity: UIActivityIndicator,
-//   wave: WaveIndicator,
-// };
+const IndicatorTypeToComponentMap = {
+  ball: BallIndicator,
+  bar: BarIndicator,
+  dot: DotIndicator,
+  spinner: MaterialIndicator,
+  pacman: PacmanIndicator,
+  pulse: PulseIndicator,
+  skype: SkypeIndicator,
+  activity: UIActivityIndicator,
+  wave: WaveIndicator,
+};
 
-// const ActivityIndicator: React.FC<IActivityIndicatorProps> = ({
-//   color = undefined,
-//   size = "m",
-//   variant = "spinner",
-//   loading = true,
-//   ...rest
-// }) => {
-//   if (!loading) return null;
+const ActivityIndicator: React.FC<IActivityIndicatorProps> = ({
+  variant = "spinner",
+  loading = true,
+  ...rest
+}) => {
+  if (!loading) return null;
 
-//   const { theme } = useTheme();
-//   const props = useStyledProps(restyleFunctions, rest);
+  const props = useStyledProps(indicatorStyleFunctions, rest);
+  const componentSpecificProps = useComponentConfig(
+    "ActivityIndicator",
+    {
+      size: rest["size"],
+    },
+    indicatorStyleFunctions
+  );
 
-//   const { primary500 } = theme.colors;
+  return React.createElement(IndicatorTypeToComponentMap[variant], {
+    color: componentSpecificProps.style[0].color,
+    ...componentSpecificProps,
+    ...props.style,
+  });
+};
 
-//   return React.createElement(IndicatorTypeToComponentMap[variant], {
-//     color: color ? color : primary500,
-//     size:
-//       variant === "dot"
-//         ? theme.activityIndicatorSize[size].size / 5
-//         : theme.activityIndicatorSize[size].size,
-//     ...props,
-//   });
-// };
-
-// export default ActivityIndicator;
+export default ActivityIndicator;
--- src/theme/src/themeFunctions.ts
+++ src/theme/src/themeFunctions.ts
@@ -1,5 +1,5 @@
-import { IBasePearlTheme } from "./types";
+import { BasePearlTheme } from "./types";
 
 // Enforces proper shape for theme without throwing away the user specific values
-export const createTheme = <T extends IBasePearlTheme>(themeObject: T): T =>
+export const createTheme = <T extends BasePearlTheme>(themeObject: T): T =>
   themeObject;
