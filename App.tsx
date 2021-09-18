6f4439774db4eb991e5c22aa069e55eec8d49029
Rohit Agrawal <rohit.agrawal@electriphi.ai>
2021-09-18 22:15:07 +0530

    feat(elevation): added elevation config to theme and a custom prop 'boxShadow' to Box components

--- App.tsx
+++ App.tsx
@@ -19,7 +19,7 @@ const App = () => {
 
   if (haveFontsLoaded) {
     return (
-      <ThemeProvider initialColorMode="dark">
+      <ThemeProvider initialColorMode="light">
         <Storybook />
         {/* <OfflineNotice />
         <Screen>
