556647bd376aa58a71de116c3f47861f5d0a81a7
Rohit Agrawal <rohit.agrawal@electriphi.ai>
2021-11-19 17:30:14 +0530

    fix: fixing missing exports, removed the temporary offline noticebar

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,22 +2,36 @@
 export { default as Box, BoxProps } from "./Atoms/Box/Box";
 export { default as Text, TextProps } from "./Atoms/Text/Text";
 export { default as Center } from "./Atoms/Center/Center";
-export { default as Stack } from "./Atoms/Stack/Stack";
-export { default as Screen } from "./Atoms/Screen/Screen";
-export { default as Pressable } from "./Atoms/Pressable/Pressable";
-export { default as Spinner } from "./Atoms/Spinner/Spinner";
-export { default as Icon } from "./Atoms/Icon/Icon";
-export { default as Divider } from "./Atoms/Divider/Divider";
+export { default as Stack, StackProps } from "./Atoms/Stack/Stack";
+export { default as Screen, ScreenProps } from "./Atoms/Screen/Screen";
+export {
+  default as Pressable,
+  PressableProps,
+} from "./Atoms/Pressable/Pressable";
+export { default as Spinner, SpinnerProps } from "./Atoms/Spinner/Spinner";
+export { default as Icon, IconProps } from "./Atoms/Icon/Icon";
+export { default as Divider, DividerProps } from "./Atoms/Divider/Divider";
 
 // Molecules
-export { default as Button } from "./Molecules/Button/Button";
-export { default as TextLink } from "./Molecules/TextLink/TextLink";
-export { default as Input } from "./Molecules/Input/Input";
-export { default as CheckBox } from "./Molecules/CheckBox/CheckBox";
-export { default as CheckBoxGroup } from "./Molecules/CheckBox/CheckBoxGroup";
-export { default as Radio } from "./Molecules/Radio/Radio";
-export { default as RadioGroup } from "./Molecules/Radio/RadioGroup";
-export { default as Image } from "./Molecules/Image/Image";
-// export { default as Badge } from "./Molecules/Badge/Badge";
-// export { default as withBadge } from "./Molecules/Badge/withBadge";
-// export { default as OfflineNotice } from "./Molecules/OfflineNotice/OfflineNotice";
+export { default as Button, ButtonProps } from "./Molecules/Button/Button";
+export {
+  default as TextLink,
+  TextLinkProps,
+} from "./Molecules/TextLink/TextLink";
+export { default as Input, InputProps } from "./Molecules/Input/Input";
+export {
+  default as CheckBox,
+  CheckBoxProps,
+} from "./Molecules/CheckBox/CheckBox";
+export {
+  default as CheckBoxGroup,
+  CheckBoxGroupProps,
+} from "./Molecules/CheckBox/CheckBoxGroup";
+export { default as Radio, RadioProps } from "./Molecules/Radio/Radio";
+export {
+  default as RadioGroup,
+  RadioGroupProps,
+} from "./Molecules/Radio/RadioGroup";
+export { default as Image, ImageProps } from "./Molecules/Image/Image";
+export { default as Badge, BadgeProps } from "./Molecules/Badge/Badge";
+export { default as withBadge } from "./Molecules/Badge/withBadge";
