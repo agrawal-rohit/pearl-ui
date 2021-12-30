33f69331abb188ea30029d6cb335955d51db93de
Rohit Agrawal <rohit.agrawal@electriphi.ai>
2021-12-31 02:10:58 +0530

    chore(typings): updating package.json typings flag and fixing linting errors

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,38 +1,38 @@
 // Atoms
-export { default as Box, type BoxProps } from "./Atoms/Box/Box";
-export { default as Text, type TextProps } from "./Atoms/Text/Text";
+export { default as Box, BoxProps } from "./Atoms/Box/Box";
+export { default as Text, TextProps } from "./Atoms/Text/Text";
 export { default as Center } from "./Atoms/Center/Center";
-export { default as Stack, type StackProps } from "./Atoms/Stack/Stack";
-export { default as Screen, type ScreenProps } from "./Atoms/Screen/Screen";
+export { default as Stack, StackProps } from "./Atoms/Stack/Stack";
+export { default as Screen, ScreenProps } from "./Atoms/Screen/Screen";
 export {
   default as Pressable,
   PressableProps,
 } from "./Atoms/Pressable/Pressable";
-export { default as Spinner, type SpinnerProps } from "./Atoms/Spinner/Spinner";
-export { default as Icon, type IconProps } from "./Atoms/Icon/Icon";
-export { default as Divider, type DividerProps } from "./Atoms/Divider/Divider";
+export { default as Spinner, SpinnerProps } from "./Atoms/Spinner/Spinner";
+export { default as Icon, IconProps } from "./Atoms/Icon/Icon";
+export { default as Divider, DividerProps } from "./Atoms/Divider/Divider";
 
 // Molecules
-export { default as Button, type ButtonProps } from "./Molecules/Button/Button";
+export { default as Button, ButtonProps } from "./Molecules/Button/Button";
 export {
   default as TextLink,
-  type TextLinkProps,
+  TextLinkProps,
 } from "./Molecules/TextLink/TextLink";
-export { default as Input, type InputProps } from "./Molecules/Input/Input";
+export { default as Input, InputProps } from "./Molecules/Input/Input";
 export {
   default as CheckBox,
-  type CheckBoxProps,
+  CheckBoxProps,
 } from "./Molecules/CheckBox/CheckBox";
 export {
   default as CheckBoxGroup,
-  type CheckBoxGroupProps,
+  CheckBoxGroupProps,
 } from "./Molecules/CheckBox/CheckBoxGroup";
-export { default as Radio, type RadioProps } from "./Molecules/Radio/Radio";
+export { default as Radio, RadioProps } from "./Molecules/Radio/Radio";
 export {
   default as RadioGroup,
-  type RadioGroupProps,
+  RadioGroupProps,
 } from "./Molecules/Radio/RadioGroup";
-export { default as Image, type ImageProps } from "./Molecules/Image/Image";
-export { default as Badge, type BadgeProps } from "./Molecules/Badge/Badge";
+export { default as Image, ImageProps } from "./Molecules/Image/Image";
+export { default as Badge, BadgeProps } from "./Molecules/Badge/Badge";
 export { default as withBadge } from "./Molecules/Badge/withBadge";
-export { default as Avatar, type AvatarProps } from "./Molecules/Avatar/Avatar";
+export { default as Avatar, AvatarProps } from "./Molecules/Avatar/Avatar";
