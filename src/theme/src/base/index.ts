2f5716e489e431dea24288f32b13b333a6d38eb6
Rohit Agrawal <rohitagrawalofficialmail@gmail.com>
2021-12-31 05:14:25 +0530

    fix: fake fix commit to trigger test release to npm

--- src/theme/src/base/index.ts
+++ src/theme/src/base/index.ts
@@ -11,21 +11,6 @@ import {
   fontWeights,
   lineHeights,
 } from "./typography";
-
-// Component configs
-import SpinnerConfig from "../../../components/Atoms/Spinner/Spinner.config";
-import ScreenConfig from "../../../components/Atoms/Screen/Screen.config";
-import TextConfig from "../../../components/Atoms/Text/Text.config";
-import ButtonConfig from "../../../components/Molecules/Button/Button.config";
-import IconConfig from "../../../components/Atoms/Icon/Icon.config";
-import TextLinkConfig from "../../../components/Molecules/TextLink/TextLink.config";
-import InputConfig from "../../../components/Molecules/Input/Input.config";
-import DividerConfig from "../../../components/Atoms/Divider/Divider.config";
-import CheckBoxConfig from "../../../components/Molecules/CheckBox/CheckBox.config";
-import ImageConfig from "../../../components/Molecules/Image/Image.config";
-import RadioConfig from "../../../components/Molecules/Radio/Radio.config";
-import BadgeConfig from "../../../components/Molecules/Badge/Badge.config";
-import AvatarConfig from "../../../components/Molecules/Avatar/Avatar.config";
 import { merge } from "lodash";
 
 import {
